f4d8ee2702e58b9095cce83c8af6302bd6b8cb9b
Tory <76063134+OsatoVicTory@users.noreply.github.com>
2022-10-21 07:52:36 +0100

    Update sendCookie.js

--- controller/sendCookie.js
+++ controller/sendCookie.js
@@ -1,8 +1,9 @@
 exports.sendCookie = (res, data) => {
     res.cookie("PRIDE_TOKEN", data, {
         httpOnly: true,
-        maxAge: Date.now() + 86400000,
+        maxAge: 2680000000,
         sameSite: 'none',
-        secure: false
+        secure: true,
+        domain: 'pride-dot-app.netlify.app'
     })
 }
